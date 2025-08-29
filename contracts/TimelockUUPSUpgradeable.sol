9fefbe17ba55994737e7d40e969fec4fa329f146
molalign8468 <molaligngetahun229@gmail.com>
2025-08-29 03:57:25 +0300

    feat: add upgradeable Timelock controller contract

--- contracts/TimelockUUPSUpgradeable.sol
+++ contracts/TimelockUUPSUpgradeable.sol
@@ -1,5 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract TimelockUUPSUpgradeable {
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {TimelockControllerUpgradeable} from "@openzeppelin/contracts-upgradeable/governance/TimelockControllerUpgradeable.sol";
+
+contract TimelockUUPSUpgradeable is
+    Initializable,
+    TimelockControllerUpgradeable,
+    OwnableUpgradeable,
+    UUPSUpgradeable
+{
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+
+    function initializeTimelock(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors,
+        address initialOwner
+    ) public initializer {
+        __TimelockController_init(minDelay, proposers, executors, initialOwner);
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    uint256[50] private __gap;
 }
