7e3120486a10f4394e808d6a6fb93f4d61351a2f
molalign8468 <molaligngetahun229@gmail.com>
2025-09-04 02:09:55 +0300

    chore: add Holesky network and Etherscan API key to Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "cancun",
+    },
+  },
+  networks: {
+    holesky: {
+      url: process.env.ALCHEMY_HOLESKY_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [`0x${process.env.PRIVATE_KEY}`] : [],
+      chainId: 17000,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
