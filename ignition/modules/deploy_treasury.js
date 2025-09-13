b1fbc6339df33ab4ce4e8af3b6557a61f2e1f55a
molalign8468 <molaligngetahun229@gmail.com>
2025-09-14 01:55:41 +0300

    deploy: add Treasury contract deployment script

---
+++ ignition/modules/deploy_treasury.js
@@ -0,0 +1,9 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+module.exports = buildModule("Treasury", (m) => {
+  const Treasury = m.contract("Treasury");
+  return { Treasury };
+});
